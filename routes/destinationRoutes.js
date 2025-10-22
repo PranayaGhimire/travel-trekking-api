da57797b6522d02ae1dc957022a1458a89f58129
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-10-22 22:49:43 +0545

    Admin Protect in destination

--- routes/destinationRoutes.js
+++ routes/destinationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createDestination, deleteDestination, getAllDestinations, getDestinationById, updateDestination } from "../controllers/destinationController.js";
 import {upload} from "../utils/cloudinary.js";
+import { adminOnly } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.post("/",upload.single("image"),createDestination);
+router.post("/",upload.single("image"),adminOnly,createDestination);
 router.get("/",getAllDestinations);
 router.get("/:id",getDestinationById);
 router.put("/:id",updateDestination);
