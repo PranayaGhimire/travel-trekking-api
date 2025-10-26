4df16a14d2de10b9788de439bbb0d159cdc9fed8
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-10-26 10:11:28 +0545

    Admin protection in package route

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -4,10 +4,10 @@ import { adminOnly, protect } from "../middlewares/authMiddleware.js";
 import { upload } from "../utils/cloudinary.js";
 const router = express.Router();
 
-router.post('/',protect,createPackage);
-router.get('/',protect,getAllPackages);
-router.get('/:id',protect,getPackageById);
-router.put('/:id',protect,updatePackage);
+router.post('/',protect,adminOnly,createPackage);
+router.get('/',getAllPackages);
+router.get('/:id',getPackageById);
+router.put('/:id',protect,adminOnly,updatePackage);
 router.delete('/:id',protect,adminOnly,deletePackage);
 router.post('/upload',upload.single('image'), (req,res) => {
     res.json({imageUrl:req.file.path});
