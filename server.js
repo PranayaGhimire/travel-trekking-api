ae697e01c2d881b6a7d5bf0fbd82872e6b8cdd6c
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2026-05-23 16:56:15 +0545

    Changed in origin

--- server.js
+++ server.js
@@ -13,17 +13,22 @@ import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
-const swaggerDoc = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+const swaggerDoc = YAML.load(path.join(process.cwd(), "swagger.yaml"));
 const app = express();
 dotenv.config();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://tts.pranayaghimire.com.np"],
+  }),
+);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req,res) => res.send(`
+app.get("/", (req, res) =>
+  res.send(`
     <h2>API Working Fine</h2>
     <h3>API Endpoints</h3>
     <h4>/api/auth/register</h4>
@@ -33,16 +38,17 @@ app.get("/", (req,res) => res.send(`
     <h4>/api/destinations</h4>
     <h4>/api/packages</h4>
     <h4>/api/payment</h4>
-    `))
+    `),
+);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/bookings",bookingRoutes);
-app.use("/api/destinations",destinationRoutes);
-app.use("/api/packages",packageRoutes);
-app.use('/api/payment',paymentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/destinations", destinationRoutes);
+app.use("/api/packages", packageRoutes);
+app.use("/api/payment", paymentRoutes);
 
-app.use('/api/docs',swaggerUi.serve,swaggerUi.setup(swaggerDoc));
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-const PORT=process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
